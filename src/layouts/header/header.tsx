54e8224ef8ce65db74846c27debfc4288c5001ed
Dilshodbek <dilshodjongulomov53@gmail.com>
2025-05-12 16:02:58 +0500

    HOC, Header & Sidebar

---
+++ src/layouts/header/header.tsx
@@ -0,0 +1,87 @@
+import { DarkLogo, EngIcons, LightLogo, RusIcons, UzbIcons } from '@/icons'
+import {
+	Box,
+	Button,
+	Flex,
+	HStack,
+	Icon,
+	IconButton,
+	Menu,
+	MenuButton,
+	MenuItem,
+	MenuList,
+	useColorMode,
+	useColorModeValue,
+} from '@chakra-ui/react'
+import Link from 'next/link'
+import { BsFillMoonFill, BsFillSunFill } from 'react-icons/bs'
+import { BiUserCircle, BiMenuAltLeft } from 'react-icons/bi'
+import { MdOutlineContactSupport } from 'react-icons/md'
+import { BsTranslate } from 'react-icons/bs'
+import { HeaderProps } from './header.props'
+const Header = ({ onToggle, toggle }: HeaderProps) => {
+	const { toggleColorMode, colorMode } = useColorMode()
+	return (
+		<Box
+			w={'full'}
+			h={'10vh'}
+			px={10}
+			pos={'fixed'}
+			top={0}
+			left={0}
+			right={0}
+			borderBottom={'1px'}
+			borderBottomColor={useColorModeValue('gray.200', 'gray.700')}
+		>
+			<Flex h={'full'} justify={'space-between'} align={'center'}>
+				<Box>
+					<HStack>
+						<Icon
+							as={BiMenuAltLeft}
+							onClick={onToggle}
+							w={6}
+							h={6}
+							cursor={'pointer'}
+						/>
+						<Link href={'/'}>
+							{colorMode === 'light' ? <DarkLogo /> : <LightLogo />}
+						</Link>
+					</HStack>
+				</Box>
+				<HStack>
+					<IconButton
+						aria-label='support'
+						icon={<MdOutlineContactSupport />}
+						colorScheme='blue'
+						variant={'ghost'}
+					/>
+					<Menu>
+						<MenuButton
+							as={IconButton}
+							icon={<BsTranslate />}
+							colorScheme='blue'
+							variant={'solid'}
+						/>
+						<MenuList>
+							<MenuItem icon={<UzbIcons />}>UZB</MenuItem>
+							<MenuItem icon={<RusIcons />}>RUS</MenuItem>
+							<MenuItem icon={<EngIcons />}>ENG</MenuItem>
+						</MenuList>
+					</Menu>
+					<IconButton
+						aria-label='color-mode'
+						icon={
+							colorMode === 'light' ? <BsFillMoonFill /> : <BsFillSunFill />
+						}
+						colorScheme={'blue'}
+						onClick={toggleColorMode}
+						variant={'outline'}
+					/>
+					<Button rightIcon={<BiUserCircle />}>Login</Button>
+				</HStack>
+			</Flex>
+		</Box>
+	)
+}
+
+export default Header
---
+++ src/layouts/layout.tsx
@@ -0,0 +1,38 @@
+import { Box } from '@chakra-ui/react'
+import { LayoutProps } from './layout.props'
+import Header from './header/header'
+import { FunctionComponent, useState } from 'react'
+import Sidebar from './sidebar/sidebar'
+
+const Layout = ({ children }: LayoutProps): JSX.Element => {
+	const [toggle, setToggle] = useState<boolean>(false)
+
+	const onToggle = () => setToggle(prev => !prev)
+	return (
+		<Box maxW={'full'} overflow={'hidden'}>
+			<Header onToggle={onToggle} /> {/* width: 100%, height: 10vh */}
+			<Sidebar toggle={toggle} /> {/* width: 300px, height: 90vh */}
+			<Box
+				mt={'11vh'}
+				pl={{ base: 0, lg: '320px' }}
+				transition={'all .4s ease'}
+			>
+				{children}
+			</Box>
+		</Box>
+	)
+}
+
+export default Layout
+
+export const withLayout = <T extends Record<string, unknown>>(
+	Component: FunctionComponent<T>
+) => {
+	return function withLayoutComponent(props: T): JSX.Element {
+		return (
+			<Layout>
+				<Component {...props} />
+			</Layout>
+		)
+	}
+}
