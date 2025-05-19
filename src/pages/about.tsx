b56b0dc1ffc41304364ccf267dc37ba7a2f8b79c
Dilshodbek <dilshodjongulomov53@gmail.com>
2025-05-19 17:44:23 +0500

    About page

---
+++ src/pages/about.tsx
@@ -0,0 +1,8 @@
+import { withLayout } from '@/layouts/layout'
+import { AboutPageComponent } from '@/page-component'
+
+const AboutPage = () => {
+	return <AboutPageComponent />
+}
+
+export default withLayout(AboutPage)
