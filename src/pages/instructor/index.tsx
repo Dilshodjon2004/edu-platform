ad0c69956277a007f332fa13ba5a50aeedef3fd4
Dilshodbek <dilshodjongulomov53@gmail.com>
2025-06-21 21:54:26 +0500

    Form field & Form validation

--- src/pages/instructor/index.tsx
+++ src/pages/instructor/index.tsx
@@ -1,7 +1,17 @@
 import { withInstructorLayout } from '@/layouts/instructor'
+import { GetServerSideProps } from 'next'
 
 const InstructorPage = () => {
 	return <div>InstructorPage</div>
 }
 
 export default withInstructorLayout(InstructorPage)
+
+export const getServerSideProps: GetServerSideProps = async () => {
+	return {
+		redirect: {
+			destination: '/instructor/students',
+			permanent: false,
+		},
+	}
+}
