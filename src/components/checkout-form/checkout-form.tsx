a0d34feaa134d7fb756fc8d1c72ab5e8de8d2a22
Dilshodbek <dilshodjongulomov53@gmail.com>
2025-07-12 13:23:17 +0500

    Payment method & Receive email

--- src/components/checkout-form/checkout-form.tsx
+++ src/components/checkout-form/checkout-form.tsx
@@ -1,90 +1,184 @@
 import {
 	AddressElement,
+	CardCvcElement,
+	CardExpiryElement,
+	CardNumberElement,
 	PaymentElement,
 	useElements,
 	useStripe,
 } from '@stripe/react-stripe-js'
 import React, { FormEvent, useEffect, useState } from 'react'
 import ErrorAlert from '../error-alert/error-alert'
-import { Button } from '@chakra-ui/react'
+import {
+	Box,
+	Button,
+	Flex,
+	Stack,
+	useColorMode,
+	useColorModeValue,
+} from '@chakra-ui/react'
 import { getTotalPrice } from '@/helpers/total-price.helper'
 import { useTypedSelector } from '@/hooks/useTypedSelector'
+import {
+	StripeAddressElement,
+	StripeCardNumberElement,
+} from '@stripe/stripe-js'
+import $axios from '@/api/axios'
+import { useRouter } from 'next/router'
+import { useActions } from '@/hooks/useActions'
+import { getMailUrl } from '@/config/api.config'
 
-export default function CheckoutForm() {
+const CheckoutForm = () => {
 	const stripe = useStripe()
 	const elements = useElements()
 
-	const [message, setMessage] = useState<string | null>(null)
+	const [error, setError] = useState<string>('')
 	const [isLoading, setIsLoading] = useState<boolean>(false)
 
 	const { courses, books } = useTypedSelector(state => state.cart)
+	const { colorMode } = useColorMode()
+	const router = useRouter()
+	const { getBooks } = useActions()
 
-	useEffect(() => {
-		if (!stripe) {
-			return
-		}
+	const cardStyles = {
+		base: {
+			color: colorMode === 'light' ? '#000' : '#fff',
+			fontSmoothing: 'antialiased',
+			fontSize: '16px',
+			'::placeholder': {
+				color:
+					colorMode === 'light' ? 'rgba(0,0,0,.5)' : 'rgba(255,255,255,.4)',
+				opacity: '0.7',
+			},
+		},
+		ivalid: {
+			color: '#fa755a',
+			iconColor: '#fa755a',
+		},
+	}
 
-		const clientSecret = new URLSearchParams(window.location.search).get(
-			'payment_intent_client_secret'
-		)
-
-		if (!clientSecret) {
-			return
-		}
-
-		stripe.retrievePaymentIntent(clientSecret).then(({ paymentIntent }) => {
-			switch (paymentIntent?.status) {
-				case 'succeeded':
-					setMessage('Payment succeeded!')
-					break
-				case 'processing':
-					setMessage('Your payment is processing.')
-					break
-				case 'requires_payment_method':
-					setMessage('Your payment was not successful, please try again.')
-					break
-				default:
-					setMessage('Something went wrong.')
-					break
-			}
-		})
-	}, [stripe])
-
-	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-		e.preventDefault()
-
-		if (!stripe || !elements) {
-			return
-		}
+	const handleSubmit = async () => {
+		if (!stripe || !elements) return
 
 		setIsLoading(true)
 
-		const { error } = await stripe.confirmPayment({
-			elements,
-			confirmParams: {
-				return_url: 'http://localhost:3000/shop/success',
+		const addressElement = elements.getElement(
+			'address'
+		) as StripeAddressElement
+
+		const { value } = await addressElement.getValue()
+
+		const cleanAddress = {
+			...value.address,
+			line2: value.address.line2 ?? undefined,
+		}
+
+		const { error, paymentMethod } = await stripe.createPaymentMethod({
+			type: 'card',
+			card: elements.getElement(CardNumberElement) as StripeCardNumberElement,
+			billing_details: {
+				address: cleanAddress,
+				name: `${value.firstName} ${value.lastName}`,
 			},
 		})
 
-		if (error.type === 'card_error' || error.type === 'validation_error') {
+		if (error) {
+			setError(`Your payment details couldn't be verified: ${error.message}`)
 			console.log(error)
-
-			setMessage(error.message as string)
+			setIsLoading(false)
 		} else {
-			console.log(error)
+			const { data } = await $axios.post(`/payment/books`, {
+				price: getTotalPrice(courses, books),
+				paymentMethod: paymentMethod.id,
+			})
 
-			setMessage('An unexpected error occurred.')
+			const payload = await stripe.confirmCardPayment(data)
+
+			if (payload.error) {
+				setIsLoading(false)
+				setError(
+					`Your payment details couldn't be verified: ${payload.error.message}`
+				)
+			} else {
+				for (const book of books) {
+					await $axios.post(`${getMailUrl('books')}/${book._id}`)
+				}
+				getBooks([])
+				router.push('/shop/success')
+			}
+			setIsLoading(false)
 		}
-
-		setIsLoading(false)
 	}
-
 	return (
-		<form id='payment-form' onSubmit={handleSubmit}>
-			{message && (
-				<ErrorAlert title={message} clearHandler={() => setMessage(null)} />
-			)}
-			<PaymentElement id='payment-element' options={{ layout: 'tabs' }} />
+		<Stack>
+			{error && <ErrorAlert title={error} clearHandler={() => setError('')} />}
+			<Flex gap={2}>
+				<Box
+					px={2}
+					py={3}
+					w={'60%'}
+					boxShadow={
+						colorMode === 'dark'
+							? '0px 2px 4px rgba(0, 0, 0, 0.5), 0px 1px 6px rgba(0, 0, 0, 0.25)'
+							: ''
+					}
+					borderRadius={'md'}
+					border={'1px'}
+					borderColor={useColorModeValue(
+						'rgba(0,0,0,.1)',
+						'rgba(255,255,255,.1)'
+					)}
+					bg={useColorModeValue('white', '#30303d')}
+				>
+					<CardNumberElement
+						options={{
+							style: cardStyles,
+							placeholder: 'XXXX XXXX XXXX XXXX',
+							showIcon: true,
+						}}
+					/>
+				</Box>
+				<Box
+					px={2}
+					w='20%'
+					py={3}
+					boxShadow={
+						colorMode === 'dark'
+							? '0px 2px 4px rgba(0, 0, 0, 0.5), 0px 1px 6px rgba(0, 0, 0, 0.25)'
+							: ''
+					}
+					borderRadius={'md'}
+					border={'1px'}
+					borderColor={useColorModeValue(
+						'rgba(0,0,0,.1)',
+						'rgba(255,255,255,.1)'
+					)}
+					bg={useColorModeValue('white', '#30303d')}
+				>
+					<CardExpiryElement options={{ style: cardStyles }} />
+				</Box>
+				<Box
+					px={2}
+					w='20%'
+					py={3}
+					boxShadow={
+						colorMode === 'dark'
+							? '0px 2px 4px rgba(0, 0, 0, 0.5), 0px 1px 6px rgba(0, 0, 0, 0.25)'
+							: ''
+					}
+					borderRadius={'md'}
+					border={'1px'}
+					borderColor={useColorModeValue(
+						'rgba(0,0,0,.1)',
+						'rgba(255,255,255,.1)'
+					)}
+					bg={useColorModeValue('white', '#30303d')}
+				>
+					<CardCvcElement
+						options={{ style: cardStyles, placeholder: 'Security code' }}
+					/>
+				</Box>
+			</Flex>
 			<AddressElement options={{ mode: 'billing' }} />
 			<Button
 				w={'full'}
@@ -93,7 +187,7 @@ export default function CheckoutForm() {
 				isDisabled={isLoading || !stripe || !elements}
 				isLoading={isLoading}
 				boxShadow={'xl'}
-				type={'submit'}
+				onClick={handleSubmit}
 			>
 				Pay now{' '}
 				{getTotalPrice(courses, books).toLocaleString('en-US', {
@@ -101,6 +195,8 @@ export default function CheckoutForm() {
 					currency: 'USD',
 				})}
 			</Button>
-		</form>
+		</Stack>
 	)
 }
+
+export default CheckoutForm
--- src/page-component/shop-page-component/checkout-page-component.tsx
+++ src/page-component/shop-page-component/checkout-page-component.tsx
@@ -10,35 +10,23 @@ import {
 	Grid,
 	GridItem,
 	HStack,
-	Skeleton,
-	Stack,
 	Text,
+	useColorMode,
 	useColorModeValue,
 } from '@chakra-ui/react'
 import { Elements } from '@stripe/react-stripe-js'
 import { loadStripe } from '@stripe/stripe-js'
 import Image from 'next/image'
-import { Fragment, useEffect, useState } from 'react'
+import { Fragment } from 'react'
 
 const stripePromise = loadStripe(
 	process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string
 )
 
 const CheckoutPageComponent = () => {
-	const [clientSecret, setClientSecret] = useState('')
+	const { books } = useTypedSelector(state => state.cart)
+	const { colorMode } = useColorMode()
 
-	const { books, courses } = useTypedSelector(state => state.cart)
-
-	useEffect(() => {
-		const getClientSecret = async () => {
-			const response = await PaymentService.paymentBooks(
-				getTotalPrice(courses, books)
-			)
-			setClientSecret(response)
-		}
-
-		getClientSecret()
-	}, [])
 	return (
 		<>
 			<SectionTitle
@@ -50,25 +38,14 @@ const CheckoutPageComponent = () => {
 			<Grid gridTemplateColumns={'70% 30%'} gap={5}>
 				<GridItem>
 					<Divider my={5} />
-					{clientSecret ? (
-						<Elements
-							stripe={stripePromise}
-							options={{ clientSecret, appearance: { theme: 'night' } }}
-						>
-							<CheckoutForm />
-						</Elements>
-					) : (
-						<Stack>
-							<HStack mt={10}>
-								<Skeleton height={12} w={'50%'} />
-								<Skeleton height={12} w={'25%'} />
-								<Skeleton height={12} w={'25%'} />
-							</HStack>
-							<Skeleton height={12} w={'100%'} mt={5} />
-							<Skeleton height={12} w={'100%'} mt={5} />
-							<Skeleton height={14} w={'100%'} mt={5} />
-						</Stack>
-					)}
+					<Elements
+						stripe={stripePromise}
+						options={{
+							appearance: { theme: colorMode === 'dark' ? 'night' : 'stripe' },
+						}}
+					>
+						<CheckoutForm />
+					</Elements>
 				</GridItem>
 				<GridItem
 					mt={10}
